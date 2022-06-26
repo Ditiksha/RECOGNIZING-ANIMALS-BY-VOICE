4e6c8804950aceb5f415986adc87ff14dc1452c5
Ditiksha <98809244+Ditiksha@users.noreply.github.com>
2022-06-26 07:40:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-//https://teachablemachine.withgoogle.com/models/WFZQ2FTeM/
+function startClassification() {
+    navigator.mediaDevices.getUserMedia({ audio: true });
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/WFZQ2FTeM/model.json', modelReady);
+}
+
+function modelReady() {
+    classifier.classify(gotResults);
+}
+
+function gotResults(error, result) {
+    console.log("Got Results");
+}
